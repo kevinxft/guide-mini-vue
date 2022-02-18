cae2b64e05b7829546c94c4deb86b7e7df4d3ae6
kevinxft <kevinxft@gmail.com>
2022-02-18 19:54:16 +0800

    parse element

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,8 @@
 import { NodeTypes } from "./ast";
-
+const enum TagType {
+  Start,
+  End,
+}
 export function baseParse(content: string) {
   const context = createParserContext(content);
   return createRoot(parseChildren(context));
@@ -10,9 +13,37 @@ function parseChildren(context) {
   let node;
   if (context.source.startsWith("{{")) {
     node = parseInterpolation(context);
+  } else if (context.source[0] === "<") {
+    if (/[a-z]/i.test(context.source[1])) {
+      node = parseElement(context);
+    }
   }
   nodes.push(node);
   return nodes;
+}
+
+function parseElement(context) {
+  const element = parseTag(context, TagType.Start);
+  parseTag(context, TagType.End);
+  console.log("context", context);
+  return element;
+}
+
+function parseTag(context: any, type: TagType) {
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+  const tag = match[1];
+
+  advanceBy(context, match[0].length);
+  advanceBy(context, 1);
+
+  if (type === TagType.End) {
+    return;
+  }
+
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+  };
 }
 
 function parseInterpolation(context) {
@@ -26,7 +57,7 @@ function parseInterpolation(context) {
   const rawContentLength = closeIndex - openDelimiter.length;
 
   const rawContent = context.source.slice(0, rawContentLength);
-  const content = rawContent.trim()
+  const content = rawContent.trim();
 
   console.log("content: ", content);
 
